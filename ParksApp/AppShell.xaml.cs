e2387c9df8496b1032a1798f76181adb7ef37819
A00233642 <Taiwoakintunde26@gmail.com>
2021-09-27 05:34:38 -0400

    Initial Blank Application
    This is the first iteration of our blank Xamarin application

---
+++ ParksApp/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ParksApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+      
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ ParksApp/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ParksApp.ViewModels
+{
+    public class WelcomeViewModel : BaseViewModel
+    {
+        public WelcomeViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://www,google.com"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
